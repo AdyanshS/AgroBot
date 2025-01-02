cd09593763ca1244bdc041bcd6a3f06c92265c34
Adyansh04 <gupta.adyansh@gmail.com>
2025-01-03 00:26:42 +0530

    Updated pin definitions for servo motors and sensors; added limit and momentary switch configurations.

--- agrobot_firmware_s3/include/pin_map.hpp
+++ agrobot_firmware_s3/include/pin_map.hpp
@@ -1,10 +1,10 @@
 
 // Servo Motor pins definitions
-#define SERVO1 11
-#define SERVO2 12
-#define SERVO3 13
-#define SERVO4 14
-#define SERVO5 15
+#define SERVO1 3
+#define SERVO2 8
+#define SERVO3 18
+#define SERVO4 17
+#define SERVO5 16
 
 // ADC Channels definitions - Cobra Line Sensor
 #define CobraIR1 ADC1_CHANNEL_3 // GPIO 4
@@ -13,9 +13,25 @@
 #define CobraIR4 ADC1_CHANNEL_6 // GPIO 7
 
 // UltraSonic Sensors
-#define echo1 x
-#define trig1 y
+#define Trig1 1 // GPIO 1
+#define Echo1 2 // GPIO 2
+#define Echo2 42 // GPIO 42 - MTMS
+#define Echo3 41 // GPIO 41 - MTDI
+#define Echo4 40 // GPIO 44 - MTCK
 
 // Sharp Ir Sensor
-#define SharpIR1 ADC1_CHANNEL_2
-#define SharpIR2 ADC2_CHANNEL_0
+#define SharpIR1 ADC1_CHANNEL_3 // GPIO 14
+#define SharpIR2 ADC2_CHANNEL_2 // GPIO 13
+#define SharpIR3 ADC2_CHANNEL_1 // GPIO 12
+#define SharpIR4 ADC2_CHANNEL_0 // GPIO 11  
+
+// Limit Switches
+#define LimitSwitch1 37 // GPIO 37
+#define LimitSwitch2 36 // GPIO 36
+#define LimitSwitch3 35 // GPIO 35
+
+// Momentary Switch
+#define MomentarySwitch1 48 // GPIO 48
+#define MomentarySwitch2 47 // GPIO 47
+#define MomentarySwitch3 21 // GPIO 21
+ 
