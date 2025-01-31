45401d0c01f07e2fd151b36a0360f70b3ee294d4
Adyansh04 <gupta.adyansh@gmail.com>
2025-01-31 18:35:41 +0530

    Add agrobot_localization package with EKF launch and configuration files

---
+++ agrobot_localization/launch/ekf_launch.py
@@ -0,0 +1,25 @@
+"""
+This launch file initializes and launches the Extended Kalman Filter (EKF) node for robot localization.
+"""
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    ekf_config = PathJoinSubstitution([
+        FindPackageShare("agrobot_localization"), "config", "ekf.yaml"
+    ])
+
+    return LaunchDescription([
+        Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_filter_node',
+            output='screen',
+            parameters=[ekf_config]
+        )
+    ])
